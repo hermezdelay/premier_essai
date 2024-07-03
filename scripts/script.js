567b17d6f86443dc7cfeb32649832b9656189398
dalil16 <dalil.hermez@systhen.com>
2024-07-03 20:57:25 +0100

    ajout d'un text quelqonque

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
 let baliseZoneProposition = document.getElementById("zoneProposition");
 console.log("salam");
 console.log(document.getElementById("zoneProposition"));
+baliseZoneProposition = document.querySelector("#zoneProposition span");
+console.log(baliseZoneProposition);
