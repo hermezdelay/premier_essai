881ce8c78169837b536bd44f1e050a7df8f143f3
dalil16 <dalil.hermez@systhen.com>
2024-07-03 15:13:01 +0100

    ajout d'un text quelqonque

---
+++ scripts/scripts.js
@@ -0,0 +1,6 @@
+let baliseZoneProposition = document.getElementById("zoneProposition");
+console.log("salam");
+console.log(baliseZoneProposition);
+
+let baliseZonePropositionSpan = document.querySelector("span");
+console.log(baliseZonePropositionSpan);
