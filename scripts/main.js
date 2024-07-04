e18caf00834ad82678d5c7ee80f2a371acef0748
dalil16 <dalil.hermez@systhen.com>
2024-07-04 19:20:17 +0100

    ajout d'un text quelqonque

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+
+let question1 = document.getElementById("nom")
+console.log(question1)
+
+
+
+
+
+
+lancerJeu()
