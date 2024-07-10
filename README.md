91fcffb9d97439e67e53b4ec88c93d9ab709a796
dalil16 <hermez.dalil@gmail.com>
2024-07-10 23:17:54 +0100

    ajout de validation de formulaire

--- README.md
+++ README.md
@@ -43,3 +43,16 @@ $ git config --list
       * git branch -M main
       * git push -u origin main
 
+/******************************************************************************************/
+
+/**
+ * Cette fonction construit et affiche l'email. 
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score. 
+ */
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
+}
+/******************************************************************************************/
---
+++ partage_resultat_popup_tp02/scripts/main.js
@@ -0,0 +1,56 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+/*
+let question1 = document.getElementById("nom")
+console.log(question1)
+
+let question2 = document.getElementById("btnValiderMot")
+question2.addEventListener("click", (event) => {
+    console.log(event)
+})
+
+
+let question3 = document.getElementById("inputEcriture")
+question3.addEventListener('keydown', (event) => {
+    console.log(event)
+
+
+})
+    
+lancerJeu()
+    /*/
+
+function change_radio(radio, mot){
+    
+    for(let i=0 ; i< radio.length ; i++){
+        console.log(radio[i])
+        radio[i].addEventListener('change', (event) => {
+
+            if(Number(radio[i].value) === 1){    
+                mot.innerText= listeMots[0]
+            }
+            else{   
+                mot.innerText= listePhrases[0]
+            }
+        })
+    }
+    
+    console.log("salam")
+
+}
+
+
+let radio = document.getElementsByName("optionSource")
+let mot = document.getElementsByName("zoneProposition")
+
+console.log(mot)
+let h1 = `<h1> salam </h1>`
+mot.innerText = `salam`
+//mot.innerText =  + listeMots[0]
+
+change_radio(radio,mot)
+initAddEventListenerPopup()
